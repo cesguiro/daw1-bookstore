08a457c5ebfe3d31d959ec307da082eedb507d5d
César Guijarro <cesar.rosaleny@gmail.com>
2024-03-21 17:52:52 +0100

    first commit

---
+++ src/main/java/es/cesguiro/daw1bookstore/Daw1BookstoreApplication.java
@@ -0,0 +1,13 @@
+package es.cesguiro.daw1bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Daw1BookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Daw1BookstoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/cesguiro/daw1bookstore/Daw1BookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package es.cesguiro.daw1bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Daw1BookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
