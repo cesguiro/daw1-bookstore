30f33a708e7f985845a6892e072ab1fe64b6ee59
César Guijarro <cesar.rosaleny@gmail.com>
2024-03-24 11:22:35 +0100

    partial commit

--- src/main/java/es/cesguiro/daw1bookstore/controller/BookController.java
+++ src/main/java/es/cesguiro/daw1bookstore/controller/BookController.java
@@ -1,2 +1,52 @@
-package es.cesguiro.daw1bookstore.controller;public class BookController {
+package es.cesguiro.daw1bookstore.controller;
+
+import es.cesguiro.daw1bookstore.domain.model.Author;
+import es.cesguiro.daw1bookstore.domain.model.Book;
+import es.cesguiro.daw1bookstore.domain.model.Publisher;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Controller
+@RequestMapping(BookController.URL)
+public class BookController {
+
+    List<Book> bookList = List.of(
+            new Book(
+                    1,
+                    "9788433920423",
+                    "La conjura de los necios",
+                    "El protagonista de esta novela es uno de los personajes más memorables...",
+                    new BigDecimal(13.20).setScale(2, BigDecimal.ROUND_HALF_UP),
+                    "necios.jpeg",
+                    new Publisher(1, "Editorial Anagrama"),
+                    List.of(
+                            new Author(1, "John Kennedy Toole")
+                    )
+            ),
+            new Book(
+                    2,
+                    "9788426418197",
+                    "El nombre de la rosa",
+                    "Valiéndose de las características de la novela gótica, la crónica medieval y la ...",
+                    new BigDecimal(12.30).setScale(2, BigDecimal.ROUND_HALF_UP),
+                    "nombreRosa.jpeg",
+                    new Publisher(2, "Penguin Random House Grupo Editorial España"),
+                    List.of(
+                            new Author(2, "Umbero Eco")
+                    )
+            )
+    );
+
+    public static final String URL = "/books";
+
+    @GetMapping
+    public String findAll(Model model) {
+        model.addAttribute("bookList", bookList);
+        return "books/list";
+    }
 }
--- src/main/java/es/cesguiro/daw1bookstore/domain/model/Book.java
+++ src/main/java/es/cesguiro/daw1bookstore/domain/model/Book.java
@@ -1,2 +1,110 @@
-package es.cesguiro.daw1bookstore.domain.model;public class Book {
+package es.cesguiro.daw1bookstore.domain.model;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Book {
+
+    private Integer id;
+    private String isbn;
+    private String title;
+    private String synopsis;
+    private BigDecimal price;
+    private String cover;
+    private Publisher publisher;
+    List<Author> authorList;
+
+    public Book(Integer id, String isbn, String title, String synopsis, BigDecimal price, String cover, Publisher publisher, List<Author> authorList) {
+        this.id = id;
+        this.isbn = isbn;
+        this.title = title;
+        this.synopsis = synopsis;
+        this.price = price;
+        this.cover = cover;
+        this.publisher = publisher;
+        this.authorList = authorList;
+    }
+
+    public Book(Integer id, String isbn, String title, String synopsis, BigDecimal price, String cover) {
+        this.id = id;
+        this.isbn = isbn;
+        this.title = title;
+        this.synopsis = synopsis;
+        this.price = price.setScale(2, RoundingMode.HALF_UP);
+        this.cover = cover;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSynopsis() {
+        return synopsis;
+    }
+
+    public void setSynopsis(String synopsis) {
+        this.synopsis = synopsis;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price.setScale(2, RoundingMode.HALF_UP);
+    }
+
+    public String getCover() {
+        return cover;
+    }
+
+    public void setCover(String cover) {
+        this.cover = cover;
+    }
+
+    public Publisher getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(Publisher publisher) {
+        this.publisher = publisher;
+    }
+
+    public List<Author> getAuthorList() {
+        return authorList;
+    }
+
+    public void setAuthorList(List<Author> authorList) {
+        this.authorList = authorList;
+    }
+
+    public void addAuthor(Author author) {
+        if (authorList == null) {
+            authorList = new ArrayList<>();
+        }
+        authorList.add(author);
+    }
 }
